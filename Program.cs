4d758665ecb74f263835de9d3acf8e40722af312
junior-developer-01 <omyhyun123@gmail.com>
2025-12-05 10:00:24 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+
+
+
+        public static void SayHello()
+        {
+            Console.WriteLine("안녕");
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            SayHello();
+        }
+
+
+    }
+}
